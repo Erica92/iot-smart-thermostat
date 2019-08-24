68d1b1ce3939009a8db5ec2716d8685f6d8a6132
sergioCap <sergio.caprara@gmail.com>
2019-08-24 12:33:45 +0200

    new dashboard

--- rpl-border-router/obj_sky/symbols.d
+++ rpl-border-router/obj_sky/symbols.d
@@ -1,2 +1,2 @@
-obj_sky/symbols.o: symbols.c symbols.h ../../../core/./loader/symbols.h
-symbols.c symbols.h ../../../core/./loader/symbols.h :
+obj_sky/symbols.o: symbols.c symbols.h ../../core/./loader/symbols.h
+symbols.c symbols.h ../../core/./loader/symbols.h :
